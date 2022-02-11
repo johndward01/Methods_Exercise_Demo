72cd3bceef06bd2c1ba1e73a0f421c478ea95244
johndward <johndward01@gmail.com>
2022-02-11 16:03:36 -0600

    Add project files.

---
+++ Methods_Exercise_Demo/Exercises.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise_Demo
+{
+    class Exercises
+    {
+        public static void Exercise1()
+        {
+            // Ask the user for their name then print to the console
+            Console.WriteLine("Please enter your name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your favorite color");
+            var color = Console.ReadLine();
+
+            Console.WriteLine("Please enter your favorite animal");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Please enter your favorite band");
+            var band = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name} and my favorite color is {color}." +
+                              $"My favorite animal is {animal} and my favorite band is {band}.");
+        }
+
+        public static void Exercise2()
+        {
+            var result = Add(10, 44);
+            Console.WriteLine(result);
+
+            var difference = Subtract(14, 19);
+            Console.WriteLine(difference);
+
+            var product = Multiply(34343, 2828);
+            Console.WriteLine(product);
+
+            var quotient = Divide(10, 2);
+            Console.WriteLine(quotient);
+        }
+
+        public static void Exercise2_Bonus()
+        {
+            var paramsResult = (Add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1));
+            Console.WriteLine(paramsResult);
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static int Add(params int[] numbers)
+        {
+            //return array.Sum();
+            int result = 0;
+
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+            return result;
+        }
+    }
+}
---
+++ Methods_Exercise_Demo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Methods_Exercise_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercises.Exercise1();
+
+            Exercises.Exercise2();
+
+            Exercises.Exercise2_Bonus();
+
+        }
+    }
+}
